64338c66645df6076e31819bb739536da5e5cf75
Problematik <102496559+Problematik1@users.noreply.github.com>
2022-06-09 02:22:54 +0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -15,15 +15,15 @@ client.on('ready', () => {
 });
 
 client.on('message', async message => {
-title(`${client.user.tag} | ${guild}`)
-let currettime = new Date();
-let curtime = currettime.getHours() + ':' + currettime.getMinutes() + ':' + currettime.getSeconds()
-if (message.author.id === client.user.id) return
-if(message.guild.id != guild) return   
-console.log('')
-console.log(colors.brightGreen(`[${message.author.tag}] [${curtime}] ${message.content}`));
-console.log(colors.brightGreen('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-'))
-}  
+    title(`${client.user.tag} | ${guild}`)
+    let currettime = new Date();
+    let curtime = currettime.getHours() + ':' + currettime.getMinutes() + ':' + currettime.getSeconds()
+    if (message.author.id === client.user.id) return
+    if (message.guild.id != guild) return
+    console.log('')
+    console.log(colors.brightGreen(`[${message.author.tag}] [${curtime}] ${message.content}`));
+    console.log(colors.brightGreen('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-'))
+}
 
 )
 
